d425c29f6cc198c59d44723551bf36f7b28b1ea9
Stefan Fuchs <s3260968@student.rmit.edu.au>
2011-08-23 13:08:10 +1000

    install script for python 2.7.2 added

---
+++ scripts/install_python_2.7_src.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Execute this script as root
+
+if [ "$UID" -ne 0 ] ; then
+   echo "You need to run this script as root (su -), or we can't install python 2.7.2 to /usr/local ... Aborting"
+   exit 1
+fi
+
+cd /usr/local/src
+wget http://python.org/ftp/python/2.7.2/Python-2.7.2.tar.bz2
+tar xvjf Python-2.7.2.tar.bz2
+cd Python-2.7.2
+
+./configure --prefix=/usr/local --with-fpectl --enable-shared --with-libc="" --with-pth --with-system-ffi
+make -j5
+make -j5 install
