7e28448193b6925f9d1d3e073d13579610e0040b
victornava <victornavav@gmail.com>
2011-08-31 20:15:54 +1000

    can set the port number from terminal

--- playground/vic/flask/bom.py
+++ playground/vic/flask/bom.py
@@ -1,10 +1,12 @@
+import sys
 from flask import Flask, make_response
 from pprint import pprint
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Try <a href="/img">img</a>'
+    html = 'Try <a href="/img">img</a> or <a href="/getmap">getmap</a>'
+    return html
 
 
 @app.route('/img')
@@ -16,13 +18,20 @@ def img():
     
 @app.route('/getmap')
 def getmap():
-    img = open('oldman.png').read()
+    img  = generate_map()
     resp = make_response(img)
     resp.headers['Content-Type'] = 'image/png'
     return resp
+    
+#Fake map generator :)    
+def generate_map():
+    return open('oldman.png').read()
 
 if __name__ == '__main__':
+    port = 5000
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
     # app.run()
 
---
+++ playground/vic/python/argv.py
@@ -0,0 +1,9 @@
+import sys
+
+port = 5000
+if len(sys.argv)  > 1:
+    port = int(sys.argv[1])
+if len(sys.argv)  > 2:
+    print "arg 2 " + sys.argv[2]
+
+print "using port %i" % port 
