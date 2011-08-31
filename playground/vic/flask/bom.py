62796ed4c20fd6d62c32f87e371a35bc1cee3150
victornava <victornavav@gmail.com>
2011-08-31 22:24:04 +1000

    more flask stuff

--- playground/vic/flask/bom.py
+++ playground/vic/flask/bom.py
@@ -1,13 +1,11 @@
 import sys
-from flask import Flask, make_response
-from pprint import pprint
+from flask import Flask, make_response, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    html = 'Try <a href="/img">img</a> or <a href="/getmap">getmap</a>'
+    html = 'Try <a href="/img">img</a> or <a href="/getmap">getmap</a> or <a href="/params">params</a>'
     return html
-
 
 @app.route('/img')
 def img():
@@ -22,11 +20,27 @@ def getmap():
     resp = make_response(img)
     resp.headers['Content-Type'] = 'image/png'
     return resp
+
+
+@app.route('/params')
+def params():
+    args = request.args
+    keys = args.keys()
+    html = "<h3>Params</h3>"
     
-#Fake map generator :)    
+    if len(keys) == 0:
+        html += 'Try passing some params like <a href="/params?fruit=mango&color=yellow&size=big">this</a><br />'
+    else:        
+        for k in keys:
+            html += k+': '+args.get(k)+'<br />'
+    return html
+    
+#Fake map generator :)
+# we'll replace this with the real method
 def generate_map():
     return open('oldman.png').read()
 
+# can pass the port number as argument
 if __name__ == '__main__':
     port = 5000
     if len(sys.argv) > 1:
@@ -34,4 +48,3 @@ if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=port)
     # app.run()
-
--- playground/vic/python/argv.py
+++ playground/vic/python/argv.py
@@ -7,3 +7,6 @@ if len(sys.argv)  > 2:
     print "arg 2 " + sys.argv[2]
 
 print "using port %i" % port 
+
+
+print(sys.argv)
