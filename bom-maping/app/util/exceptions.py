ead909af5117ee7cd64abc519ba6ed78aaf6472a
Saurabh Pandit <s3270950@student.rmit.edu.au>
2011-09-21 17:43:18 +1000

    added data source module

--- bom-maping/app/util/exceptions.py
+++ bom-maping/app/util/exceptions.py
@@ -7,3 +7,11 @@ class BBoxException(Exception):
         
     def __str__(self):
         return repr(self.message)
+        
+class NetCDFException(Exception):
+    
+    def __init__(self, message):
+        self.message = message
+        
+    def __str__(self):
+        return repr(self.message)
