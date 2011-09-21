7ed4ec8c2bd83fb4f4de085e5a93dff966d27421
victornava <victornavav@gmail.com>
2011-09-22 03:34:28 +1000

    added WMSParams class and tests

--- bom-maping/app/app.py
+++ bom-maping/app/app.py
@@ -1,12 +1,21 @@
 import sys
 from flask import Flask, make_response, request
+from modules.wms.wms_params import WMSParams
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello"
     
-    
+    try:
+        params = WMSParams(request).parse()
+        # TODO call plotting module here
+        reply = str(params) 
+    except Exception, e:
+        # TODO call WMSException here
+        reply = str(e)    
+    return reply
+        
 # can pass the port number as argument
 if __name__ == '__main__':
     port = 8007
@@ -14,6 +23,7 @@ if __name__ == '__main__':
         port = int(sys.argv[1])
     app.debug = True
     app.run(host='0.0.0.0', port=port)
-    # app.run()
 
+
+        
         
