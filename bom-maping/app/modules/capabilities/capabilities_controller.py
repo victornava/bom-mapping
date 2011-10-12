fb7334ba70c51574444c023814c82f1ef8351a9c
Vasanth Periyasamy <mail@vasanth.org>
2011-10-12 21:13:20 +1100

    Updated GetCap Controller

--- bom-maping/app/modules/capabilities/capabilities_controller.py
+++ bom-maping/app/modules/capabilities/capabilities_controller.py
@@ -1,54 +1,82 @@
 """ Capabilities Controller
 
 All functionality that has to do with getCapabilities goes into this module.
+
 """
 
+import config
 import util.exceptions as ex
-# TODO : Load config files ( address info , abstract, fee etc., )
 
-def get_capabilities(params):
-    """ Returns a capabilities in the specified format ( xml/json) """
-    cap = CapabilitiesController (params)
+
+def get_capabilities(params,defaults):
+    """ Returns a capabilities dictionary & format """
+    cap = CapabilitiesController (params,defaults)
     return cap.get_capabilities()
 
 
 class CapabilitiesController():
-    """ Capabilities Controller 
-    
+    """ Capabilities Controller
+
     This class controlls all the get capabilities related functionality.
+    Functions are based on the WMS XML Schema.
+    Ref: http://schemas.opengis.net/wms/1.3.0/capabilities_1_3_0.xml
     """
     
-    def __init__(self, params):
-        """ Constructor """
-        self.params = params
+    def __init__(self, params, defaults):
+        """ Constructor
 
-        # considering 1.3.0 as the default version
-        # self.version = '1.3.0'
+        """
+
+        self.defaults = defaults
+        
+        # lowercase all the keys/values
+        self.params = dict((k.lower(), v.lower()) for k,v in params.items())
+        
+        if 'format' not in self.params:
+            self.params['format'] = defaults['format']
+        
+        # convert 'text/xml' into 'xml' 
+        self.params['format'] = self.params['format'].split('/')[-1]
+
+        # validate verion number & if it's not specified,assign the default value
+        if "version" in self.params:
+            if self.params['version'] != config.available['version']:
+                raise ex.InvalidVersionError(self.params['version'])
+        else:
+            self.params['version'] = defaults['version']
+
+        # validate service name & if it's not specified,assign the default value
+        if "service" in self.params:
+            if self.params['service'] != config.available['service']:
+                raise ex.InvalidServiceError(self.params['service'])
+        else:
+            self.params['service'] = defaults['service']
+
+        #TODO : Validating Sequence Parameter
 
 
     def get_capabilities(self):
-
-        # TODO - Get Capabilities 
-        #( Service, Metadata, Contact .. )
         
         service = self.__get_service()
         capability = self.__get_capability()
         contents = {"service" : service, "capability" : capability}
-        return contents
+        return contents, self.params['format']
 
 
     def __get_service(self):
 
-        # TODO
-        # contact_info = self.__get_service_contactinfo()
-        
-        # Test content for getCap()
-        service = { "name": "WMS", "title": "BOM - Australia" , "abstract" : "Map Overlay Web Service for the Australian Bureu of Meteorology"}
+        service_basic_info = self.__get_service_basicinfo()
+        contact_info = self.__get_service_contactinfo()
+
+        service = service_basic_info
+        service['contact_info'] = contact_info
         return service
 
+    def __get_service_basicinfo(self):
+        return config.service_basic_info
+
     def __get_service_contactinfo(self):
-        # TODO
-        pass
+        return config.contact_info
 
     def __get_capability(self):
 
@@ -56,19 +84,17 @@ class CapabilitiesController():
         exception = self.__get_capability_exception()
         layer = self.__get_capability_layer()
         
-        # Test content for getCap()
-        capability = { "request": request, "exception:" : exception ,
+        capability = { "request": request, "exception" : exception,
                        "layer" : layer}
         return capability
 
     def __get_capability_request(self):
         # TODO
-        pass
+        return "request details"
 
     def __get_capability_exception(self):
-        # TODO
-        pass
+        return {"formats" : config.available['exception_formats']}
     
     def __get_capability_layer(self):
         # TODO
-        pass
+        return "layer details"
