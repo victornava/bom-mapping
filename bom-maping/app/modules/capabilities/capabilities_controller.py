f4602a700a3230dee8304902a6988c71e2d3d09e
Vasanth Periyasamy <mail@vasanth.org>
2011-10-25 22:55:57 +1100

    Updated Available Layer(s) Info

--- bom-maping/app/modules/capabilities/capabilities_controller.py
+++ bom-maping/app/modules/capabilities/capabilities_controller.py
@@ -4,7 +4,6 @@ All functionality that has to do with getCapabilities goes into
 this module.
 
 """
-#TODO : Tweak for performance
 
 import config
 import util.exceptions as ex
@@ -22,12 +21,11 @@ class CapabilitiesController():
     This class controlls all the get capabilities related functionality.
     Functions are based on the WMS XML Schema.
     Ref: http://schemas.opengis.net/wms/1.3.0/capabilities_1_3_0.xml
+    Ref: ../templates/capabilities_1_3_0.xml
     """
     
     def __init__(self, params, defaults):
-        """ Constructor
-
-        """
+        """ Constructor """
 
         self.defaults = defaults
         
@@ -37,31 +35,29 @@ class CapabilitiesController():
         
         if 'format' not in self.params:
             self.params['format'] = defaults['format']
-        
+
         # convert 'text/xml' into 'xml' 
         self.params['format'] = self.params['format'].split('/')[-1]
 
-        # validate verion number & if it's not specified,
-        # assign the default value
+        # validate verion number / assign the default value
         if "version" in self.params:
             if self.params['version'] != config.available['version']:
-                raise ex.InvalidVersionError(self.params['version'])
+                msg = "version '" +str(self.params['version'])+"' is not supported'"
+                raise ex.InvalidVersionError(msg)
         else:
             self.params['version'] = defaults['version']
 
-        # validate service name & if it's not specified,
-        # assign the default value
+        # validate service name / assign the default value
         if "service" in self.params:
             if self.params['service'] != config.available['service']:
-                raise ex.InvalidServiceError(self.params['service'])
+                msg = "service '" +str(self.params['service'])+"' is not supported'"
+                raise ex.InvalidServiceError(msg)
         else:
             self.params['service'] = defaults['service']
 
-        #TODO : Validating Sequence Parameter
-
-
     def get_capabilities(self):
-        
+        """ Returns capabilities dictionary and output format """
+
         service = self.__get_service()
         capability = self.__get_capability()
         contents = {"service" : service, "capability" : capability}
@@ -69,7 +65,11 @@ class CapabilitiesController():
 
 
     def __get_service(self):
+        """
+        Returns service metadata info as a dictionary
 
+        """
+        
         service_basic_info = self.__get_service_basicinfo()
         contact_info = self.__get_service_contactinfo()
 
@@ -84,7 +84,14 @@ class CapabilitiesController():
         return config.contact_info
 
     def __get_capability(self):
+        """
+        Returns WMS info,
+        * Request available,
+        * Exception formats
+        * layer & style available
+        Ref: http://www.opengeospatial.org/standards/wms
 
+        """
         requests = self.__get_capability_request()
         exception = self.__get_capability_exception()
         layers = self.__get_capability_layer()
@@ -102,14 +109,10 @@ class CapabilitiesController():
                 formats = config.available['capabilities_formats']
             else:
                 formats = config.available['image_formats']
-
-            #TODO : Convert the file format in MIME repr
-            #  ( xml to text/xml .. )
                 
             request_methods = config.available['request_methods']
 
-            #TODO : Fetch URI from service & pass here (or) use config file
-            url = 'http://0.0.0.0/'
+            url = config.service_url
             requests.update({request :
                              {"formats" : formats,
                               "request_methods" : request_methods,
@@ -121,18 +124,22 @@ class CapabilitiesController():
     
     def __get_capability_layer(self):
 
-        #TODO: Authority URL ?
-        auth_url = "http://bom.gov.au"
+        #Authority URL - reference to entity id's
+        #e.g. auth_url = "http://bom.gov.au/def/ids.."
 
-        #TODO: Check Styles
-        styles = {"name" :config.available['styles']}
+        '''
+        Available Layer(s)..
+        For layers and styles relation, ref : openGIS WMS Specification
+
+        '''
+        available_layer = {"name" : config.defaults['layers'],
+                           "title" : config.defaults['layers'],
+                           "crs" : config.defaults['crs'],
+                           "bbox" : config.defaults['bbox'].split(","),
+                           "styles" : config.available['styles']}
 
         layers = {"title":config.service_basic_info['title'],
-                  "crs":config.defaults['crs'],
-                  "auth_url": auth_url,
-                  "bbox" : config.defaults['bbox'].split(","),
-                  "styles" : styles}
-
-        #TODO : Different/Other layers ?
+                  "available_layer" : available_layer,
+                  "crs":config.defaults['crs']}
 
         return layers
